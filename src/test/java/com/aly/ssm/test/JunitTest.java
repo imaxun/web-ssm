c959e5577aabb818e31adf3c00bcb2000663cb8f
lizhen <lz728258575@163.com>
2017-09-08 17:03:10 +0800

    优化

--- src/test/java/com/aly/ssm/test/JunitTest.java
+++ src/test/java/com/aly/ssm/test/JunitTest.java
@@ -8,9 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @Author lizhen
  * @Date 2017/8/10 11:10
